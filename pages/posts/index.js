ed122805948494051b182c22d0b8f0b2a3836b39
R.V.Rajagopalan <rajagopalan@RVRajagopalans-MacBook-Pro.local>
2022-08-22 17:25:37 +0530

    added i posts2

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,25 +1,7 @@
-import Link from "next/link";
-// import { useEffect } from "react";
-// import { useRouter } from "next/router";
-
 export default function index() {
-
-    // const router = useRouter();
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         router.push('/');
-    //     }, 4000)
-    // })
-
-    return (
-        <div>
-            <h1>welcome</h1>
-            <style jsx>{`
-                h1 {
-                    color: #f01b29;
-                }
-            `}</style>
-        </div>
-    )
+  return (
+    <div>
+      posts
+    </div>
+  )
 }
